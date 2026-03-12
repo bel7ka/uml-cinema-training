139558de8c25765d6684e0b847ff968b3babe9e1
Djelil_Belkacem <belkacem.djelil@edouardgand.fr>
2026-03-12 15:56:20 +0100

    commit1 uml

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
-﻿namespace uml_cinema_training;
+﻿using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+using System.Text;
 
+namespace uml_cinema_training;
+
+public class movie
+{
+    private string title;
+    private string duration;
+    private void display()
+    {
+    }
+}
 class Program
 {
     static void Main(string[] args)
