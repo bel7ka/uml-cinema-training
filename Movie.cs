bf3694b54397ce18a4e18a3707e6642a29a96398
Djelil_Belkacem <belkacem.djelil@edouardgand.fr>
2026-03-12 16:32:44 +0100

    commit 3

---
+++ Movie.cs
@@ -0,0 +1,19 @@
+public class Movie
+{
+    private string title;
+    private string duration;
+    public void Display()
+    {
+        Console.WriteLine($"Title: {title}, Duration: {duration}");
+    }
+    public string title
+    {
+        get { return title;}
+        set { title = value;}
+    }
+    public string duration
+    {
+        get{ return duration; }
+        set { duration = value;}
+    }
+}
